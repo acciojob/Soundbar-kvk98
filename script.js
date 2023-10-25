cedab3bdaa245d2a1cf3103aad9f2dec634905ed
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-25 14:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
 function playAudio() {
 	let audio = document.getElementById("btn-1");
 	audio.play();
+	let audio = document.getElementById("btn-2");
+	audio.play();
+	let audio = document.getElementById("btn-3");
+	audio.play();
+	let audio = document.getElementById("btn-4");
+	audio.play();
+	let audio = document.getElementById("btn-5");
+	audio.play();
+	let audio = document.getElementById("btn-6");
+	audio.play();
+	let audio = document.getElementById("btn-7");
+	audio.play();
+	
 }
