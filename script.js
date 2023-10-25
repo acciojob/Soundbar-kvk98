3ee39155d6033d1c4a5529aa306a0da6a0fdc36c
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-25 15:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //your JS code here. If required.
-function playAudio() {
-	let audio = document.getElementById("btn-1");
-	btn-1.play();
-	let audio = document.getElementById("btn-2");
-	btn-2.play();
-	let audio = document.getElementById("btn-3");
-	btn-3.play();
-	let audio = document.getElementById("btn-4");
-	btn-4.play();
-	let audio = document.getElementById("btn-5");
-	btn-5.play();
-	let audio = document.getElementById("btn-6");
-	btn-6.play();
-	let audio = document.getElementById("btn-7");
-	btn-7.play();
-	
+function playAudio(audioId) {
+	const audio = new Audio(`/sounds/${audioId}.mp3`);
+	audio.play();
 }
+
+document.querySelectorAll(`.btn`).forEach(function (button) {
+	button.addEventListener("click", function () {
+		if (this.id === 'applause' || this.id === 'boo' || this.id === 'gasp' || this.id === 'tada' || this.id === 'victory' || this.id === 'wrong') {
+			playAudio(this.id);
+		} else if(this.classList.contains('stop')){
+			document.querySelectorAll('audio').forEach(function(audio) {
+                audio.pause();
+                audio.currentTime = 0;
+            });)
+		}
+	});
+});
